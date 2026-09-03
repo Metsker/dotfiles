740bd057edff3f5949a7cd1ae22d4867d42bbae4
metsker <lev.shchinoff@gmail.com>
2026-09-03 17:57:50 +0200

    apps: drop keepassxc

--- modules/apps.nix
+++ modules/apps.nix
@@ -68,8 +68,6 @@
       discord
       krita
       spotify
-      # Also the session's Secret Service provider, which is what lets noctalia persist credentials.
-      keepassxc
     ];
 
     xdg.mimeApps.defaultApplications = {
